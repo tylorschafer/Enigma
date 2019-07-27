4fd5479db5200d10f5a9dbe2f65f1a608253dfc4
Tylor Schafer <tylorschafer@gmail.com>
2019-07-27 12:57:18 -0600

    Add crypt class chracter_set functionality

--- lib/crypt.rb
+++ lib/crypt.rb
@@ -1,3 +1,12 @@
 class Crypt
+  attr_reader :character_set
+
+  def initialize
+    @character_set = generate_character_set
+  end
+
+  def generate_character_set
+    ("a".."z").to_a << " "
+  end
 
 end
--- test/crypt_test.rb
+++ test/crypt_test.rb
@@ -10,4 +10,15 @@ class CryptTest < Minitest::Test
     assert_instance_of Crypt, @crypt
   end
 
+  def test_get_character_set
+    expected = ["a", "b", "c", "d",
+                "e", "f", "g", "h",
+                "i", "j", "k", "l",
+                "m", "n", "o", "p",
+                "q", "r", "s", "t",
+                "u", "v", "w", "x",
+                "y", "z", " "]
+    assert_equal expected, @crypt.character_set
+  end
+
 end
