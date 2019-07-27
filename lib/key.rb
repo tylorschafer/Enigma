9c400225c922b7adf88c37a7df755f8ae232f143
Tylor Schafer <tylorschafer@gmail.com>
2019-07-26 20:04:38 -0600

    setup key initialization and tests

--- lib/key.rb
+++ lib/key.rb
@@ -1,3 +1,8 @@
 class Key
+  attr_reader :user_key
+
+  def initialize(user_key = nil)
+    @user_key = user_key
+  end
 
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -3,11 +3,18 @@ require_relative 'test_helper'
 class KeyTest < Minitest::Test
 
   def setup
-    @key = Key.new
+    @key_empty = Key.new
+    @key = Key.new("04876")
   end
 
   def test_it_exists
+    assert_instance_of Key, @key_empty
     assert_instance_of Key, @key
+  end
+
+  def test_attributes
+    assert_nil @key_empty.user_key
+    assert_equal "04876", @key.user_key
   end
 
 end
