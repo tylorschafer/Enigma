2ddd7cbb9cb0981833471e1b70a91d493304f612
Tylor Schafer <tylorschafer@gmail.com>
2019-07-27 14:14:08 -0600

    Add rotated_set method and test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,6 +5,11 @@ class Enigma
     @key = Key.new.random_key
     @offset = Offset.new.get_date
     @character_set = generate_character_set
+    @enrypt_output = {
+      :encryption => "",
+      :key => nil,
+      :date => nil
+    }
   end
 
   def generate_character_set
@@ -15,7 +20,25 @@ class Enigma
     Shift.new(key, offset).merge_subshifts
   end
 
-  def encrypt(message = @message, key = @key, date = @offset)
-
+  def find_index(char)
+    @character_set.index(char)
   end
+
+  def find_char(index)
+    @character_set[index]
+  end
+
+  def rotated_set(key, offset, index)
+    return @character_set.rotate(get_shifts(key,offset)[:a]) if index == 0
+    return @character_set.rotate(get_shifts(key,offset)[:b]) if index == 1
+    return @character_set.rotate(get_shifts(key,offset)[:c]) if index == 2
+    return @character_set.rotate(get_shifts(key,offset)[:d]) if index == 3
+  end
+
+  # def encrypt(message = @message, key = @key, date = @offset)
+  #   message.chars.each_with_index do |char, index|
+  #     rotated_set(index)
+  #     @encrypt_output[:encryption] <<
+  #   end
+  # end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -32,4 +32,35 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, @enigma.get_shifts("01234", "062719")
   end
 
+  def test_find_index
+    assert_equal 0, @enigma.find_index("a")
+    assert_equal 26, @enigma.find_index(" ")
+  end
+
+  def test_find_char
+    assert_equal "a", @enigma.find_char(0)
+    assert_equal " ", @enigma.find_char(26)
+  end
+
+  def test_rotated_set
+    expected =
+      ["d", "e", "f", "g",
+       "h", "i", "j", "k",
+       "l", "m", "n", "o",
+       "p", "q", "r", "s",
+       "t", "u", "v", "w",
+       "x", "y", "z", " ",
+       "a", "b", "c"]
+
+    assert_equal expected, @enigma.rotated_set("01234", "062719", 0)
+  end
+
+  def test_encrypt
+    skip
+    expected = {
+
+    }
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
 end
