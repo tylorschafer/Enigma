c0065a74eee810525b235ab447589b2c983f9ee5
Tylor Schafer <tylorschafer@gmail.com>
2019-07-27 12:45:47 -0600

    Merge_subshifts working properly

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,14 +1,17 @@
 class Shift
-  attr_reader :collected_shifts
+  attr_reader :collected_shifts, :key_shifts, :offset_shifts
 
-  def initialize
+  def initialize(key = Key.new.generate_key, offset = Offset.new.get_date)
     @collected_shifts = Hash.new(0)
-    @key = Key.new
-    @offset = Offset.new
+    @key_shifts = Key.new(key).create_key_subshifts
+    @offset_shifts = Offset.new(offset).create_offset_subshifts
   end
 
   def merge_subshifts
-
+    @key_shifts.merge(@offset_shifts) do |key, key_shift, offset_shift|
+      @collected_shifts[key] += (key_shift + offset_shift)
+    end
+    @collected_shifts
   end
 
 end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -3,9 +3,7 @@ require_relative 'test_helper'
 class ShiftTest < Minitest::Test
 
   def setup
-      @shift = Shift.new
-      @key = Key.new
-      @offset = Offset.new
+      @shift = Shift.new("01234", "062719")
   end
 
   def test_it_exists
@@ -16,8 +14,34 @@ class ShiftTest < Minitest::Test
     assert_equal ({}), @shift.collected_shifts
   end
 
-  def test_merge_subshifts
+  def test_key_shifts
+    expected =  {
+        :a => 1,
+        :b => 12,
+        :c => 23,
+        :d => 34
+      }
+    assert_equal expected, @shift.key_shifts
+  end
 
+  def test_offset_shifts
+    expected = {
+      :a => 2,
+      :b => 9,
+      :c => 6,
+      :d => 1
+    }
+    assert_equal expected, @shift.offset_shifts
+  end
+
+  def test_merge_subshifts
+    expected_1 = {
+      :a => 3,
+      :b => 21,
+      :c => 29,
+      :d => 35
+    }
+    assert_equal expected_1, @shift.merge_subshifts
   end
 
 end
