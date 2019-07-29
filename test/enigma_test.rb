4a3249004703519bacc3bd631f5f6618a0d7620c
Tylor Schafer <tylorschafer@gmail.com>
2019-07-29 13:25:58 -0600

    Add fully functional crack

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -131,4 +131,13 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
     assert_equal expected_2, @enigma.decrypt("keder ohulw!", "02715", "040895")
   end
+
+  def test_crack
+    expected = {
+    decryption: "hello world end",
+    date: "291018",
+    key: "08304"
+  }
+  assert_equal expected, @enigma.crack("vjqtbeaweqihssi", "291018")
+  end
 end
