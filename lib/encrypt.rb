6ae064700dcd6f34e615cebdaf25377a4a11c72b
Tylor Schafer <tylorschafer@gmail.com>
2019-07-30 11:33:24 -0600

    Reformat runners to store ARGV in variable to increase readability

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -3,10 +3,13 @@ require './lib/key'
 require './lib/offset'
 require './lib/shift'
 
+input_text = ARGV[0]
+encryption_output = ARGV[1]
+
 enigma = Enigma.new
 
-encryptor = enigma.encrypt(File.read(ARGV[0]))
+encryptor = enigma.encrypt(File.read(input_text))
 
-File.write(ARGV[1], encryptor[:encryption])
-puts "Created #{ARGV[1]} with the key #{encryptor[:key]}
+File.write(encryption_output, encryptor[:encryption])
+puts "Created #{encryption_output} with the key #{encryptor[:key]}
       and date #{encryptor[:date]}"
